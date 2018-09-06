4cf9ae1794fc6b439da6e9fed1ae7d2ff1e77d4a
k8hertweck <k8hertweck@gmail.com>
2018-09-05 17:15:44 -0700

    converting to clinical

--- week2.py
+++ week2.py
@@ -36,18 +36,19 @@ import pandas as pd
 # describe data and where it came from
 
 # import data as csv
-pd.read_csv("data/surveys.csv")
+pd.read_csv("data/clinical.csv")
 # this only prints it to the screen!
 
 # assign data to object
-surveys_df = pd.read_csv("data/surveys.csv")
+clinical_df = pd.read_csv("data/clinical.csv")
 
 # inspect data import
-surveys_df.head() # print top few rows
-type(surveys_df) # look at data type
-surveys_df.columns # view column names
-surveys_df.dtypes # look at type of data in each column
-surveys_df['sex'].dtype # single type; O stands for object/string
+clinical_df.head() # print top few rows
+clinical_head = clinical_df.head()
+type(clinical_df) # look at data type
+clinical_df.columns # view column names
+clinical_df.dtypes # look at type of data in each column
+clinical_df['primary_diagnosis'].dtype # single column, O stands for "object"
 
 # numeric and text data types
 # Int64 stands for 64 bit integer
@@ -58,28 +59,27 @@ surveys_df['sex'].dtype # single type; O stands for object/string
 # float64 = float
 # datetime64 = N/A
 
-# examine data type
-
-# Convert the record_id field from an integer to a float
-surveys_df['record_id'] = surveys_df['record_id'].astype('float64')
-surveys_df['record_id'].dtype
+# can convert between data types, but is difficult without dealing with missing data
+# Convert the record_id field from an float to integer
+#clinical_df['age_at_diagnosis'] = clinical_df['age_at_diagnosis'].astype('int64')
+#clinical_df['record_id'].dtype
 
 #### Inspecting and summarizing data ####
 
-pd.unique(surveys_df['species_id'])
+pd.unique(clinical_df['disease'])
 
 # calculate basic stats for all records in single column
-surveys_df['weight'].describe()
+clinical_df['age_at_diagnosis'].describe()
 
 # each metric one at a time (only prints last if all exectuted in one cell!)
-surveys_df['weight'].min()
-surveys_df['weight'].max()
-surveys_df['weight'].mean()
-surveys_df['weight'].std()
-surveys_df['weight'].count()
+clinical_df['age_at_diagnosis'].min()
+clinical_df['age_at_diagnosis'].max()
+clinical_df['age_at_diagnosis'].mean()
+clinical_df['age_at_diagnosis'].std()
+clinical_df['age_at_diagnosis'].count()
 
 # Group data by sex
-grouped_data = surveys_df.groupby('sex')
+grouped_data = clinical_df.groupby('disease')
 
 # Summary statistics for all numeric columns by sex
 grouped_data.describe()
@@ -87,23 +87,23 @@ grouped_data.describe()
 grouped_data.mean()
 
 # Count the number of samples by species
-species_counts = surveys_df.groupby('species_id')['record_id'].count()
-print(species_counts)
+disease_counts = clinical_df.groupby('disease')['race'].count()
+print(disease_counts)
 
 # count only rows with species "DO"
-surveys_df.groupby('species_id')['record_id'].count()['DO']
+clinical_df.groupby('disease')['race'].count()['white']
 
-# Multiply all weight values by 2
-surveys_df['weight']*2
+# convert columns
+clinical_df['age_at_diagnosis']/365
 
 #### Visualizing data ####
 
 # Make sure figures appear inline in Ipython Notebook
 %matplotlib inline
 # Create a quick bar chart
-species_counts.plot(kind='bar');
+disease_counts.plot(kind='bar');
 
-total_count = surveys_df.groupby('plot_id')['record_id'].nunique()
+total_count = clinical_df.groupby('disease')['site_of_resection_or_biopsy'].nunique()
 # Let's plot that too
 total_count.plot(kind='bar');
 
