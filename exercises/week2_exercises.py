9fbbb02284bbcb84a3866c8e0b33f8d41b2134ce
k8hertweck <k8hertweck@gmail.com>
2018-11-13 10:44:23 -0800

    week 2 exercises + solutions for in-class

---
+++ exercises/week2_exercises.py
@@ -0,0 +1,37 @@
+#### Intro to Python: week 2 exercises ####
+
+# Starting with data
+
+# Objectives:
+#   using packages
+#   tidy data and importing data to python
+#   selecting data using labels (columns) and rows
+#   slicing subsets of rows and columns
+#   calculating summary statistics
+
+#### In-class exercises ####
+
+## Challenge: What do you need to do to download and import the following files correctly:
+# example1: https://raw.githubusercontent.com/fredhutchio/R_intro/master/extra/clinical.tsv
+# example2: https://raw.githubusercontent.com/fredhutchio/R_intro/master/extra/clinical.txt
+
+## Challenge: does the order of the columns you list matter?
+
+## Challenge: how would you extract the last 10 rows of the dataset?
+
+## Challenge: How and why are the following three objects different?
+# Hint: try applying head()
+clinical_df
+ref_clinical_df
+true_copy_clinical_df
+
+## Challenge: why doesn't the following code work?
+#clinical_df.loc[2, 6]
+
+## Challenge: how would you extract the last 100 rows for only vital status and days to death?
+
+## Challenge: What type of summary stats do you get for object data?
+
+## Challenge: How would you extract only the standard deviation for days to death?
+
+#### Extra exercises ####
